a0059b4d370f9e9f5efc2b5f53b3933eea5abba6
JeevithaH <preetujeevi171977@gmail.com>
2023-06-24 15:02:00 +0530

    data ingestion notebook added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Text-summarize-Project
+
+##workflows
+1. update comfig.yaml
+2. update params.yaml
+3. update entity.yaml
+4. update the configuration manager in src config
+5. update component
+6. update the pipeline
+7. update the main.py
+8. update the app.py
+
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md","r",encoding="utf-8") as f:
 
 __version__="0.0.0"
 
-REPO_NAME="text-Summarizer"
+REPO_NAME="textSummarizer"
 AUTHOR_USER_NAME="JeevithaH"
-SRC_REPO="text-summarizer"
+SRC_REPO="textSummarizer"
 AUTHOR_EMAIL="jeevithah648@gmail.com"
 
 setuptools.setup(
